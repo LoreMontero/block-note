f5ed320110b957b16a37d3f76b8659d516ba1108
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2023-01-01 11:04:54 -0500

    Installed dotenv with npm.

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,4 +20,10 @@ task("balance", "Prints an account's balance")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://patient-dawn-arrow.ethereum-goerli.discover.quiknode.pro/a0c6a0e01e40486fa5c62d4fb0e7b8768496a4a2/",
+      accounts: ["0x8d1a793c84a768806af216d03d22bf7fb26504d346f945d29c937d34804c9344"]
+    }
+  }
 };
